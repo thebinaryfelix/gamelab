d769545929b224d291e53429070799ea09e8e1de
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-02-27 15:22:41 -0300

    Refactoring

--- fire/code.js
+++ fire/code.js
@@ -1,121 +1,98 @@
-(function () {
-    var $root = {};
+const ROOT = {};
 
-    function setup() {
-        $root.width = null;
-        $root.height = null;
-        $root.screen = null;
-        $root.buffer = null;
-        $root.palette = [
-            0x000000, 0x110000, 0x220000, 0x330000, 0x440000, 0x551100,
-            0x660000, 0x770000, 0x880000, 0xcc0000, 0xcc4400, 0xcc4400,
-            0xcc4422, 0xcc5511, 0xcc5566, 0xcc6644, 0xee7766, 0xee8800,
-            0xee8822, 0xee8855, 0xee9900, 0xee9922, 0xeeaa33, 0xeecc44,
-            0xeecc55, 0xeecc66, 0xeecc77, 0xffee77, 0xffee84, 0xffee90,
-            0xffee99, 0xffffaa, 0xffffbb, 0xffffcc, 0xffffdd, 0xffffff
-        ];
+const setupSeed = ($) => {
+  for (let i = $.buffer.length - $.width; i < $.buffer.length; i += 1) {
+    $.buffer[i] = $.palette.length - 1;
+  }
+};
 
-        setupScreen($root);
-        setupBuffer($root);
-        setupSeed($root);
-    }
+const setup = () => {
+  const canvas = document.getElementById('screen');
+  ROOT.screen = canvas.getContext('2d');
+  ROOT.width = parseInt(canvas.width, 10);
+  ROOT.height = parseInt(canvas.height, 10);
+  ROOT.buffer = new Uint8Array(ROOT.width * ROOT.height).fill(0);
+  ROOT.palette = [
+    0x000000, 0x110000, 0x220000, 0x330000, 0x440000, 0x551100,
+    0x660000, 0x770000, 0x880000, 0xcc0000, 0xcc4400, 0xcc4400,
+    0xcc4422, 0xcc5511, 0xcc5566, 0xcc6644, 0xee7766, 0xee8800,
+    0xee8822, 0xee8855, 0xee9900, 0xee9922, 0xeeaa33, 0xeecc44,
+    0xeecc55, 0xeecc66, 0xeecc77, 0xffee77, 0xffee84, 0xffee90,
+    0xffee99, 0xffffaa, 0xffffbb, 0xffffcc, 0xffffdd, 0xffffff
+  ];
+  setupSeed(ROOT);
+};
 
-    function mainLoop() {
-        propagate($root, 0);
-        renderBuffer($root);
-    }
 
-    window.addEventListener('load', function () {
-        setup();
-        window.mainLoopInterval = window.setInterval(mainLoop, 80);
-    });
+const calculateDecay = () => Math.floor(Math.random() * (1.15));
 
-    function setupScreen ($) {
-        canvas = document.querySelector('#screen');
-        $.width = parseInt(canvas.width);
-        $.height = parseInt(canvas.height);
-        $.screen = canvas.getContext('2d');
-    }
+const calculateBehavior = (direction) => {
+  let behavior = direction;
+  if (direction === 0) {
+    behavior = Math.floor(Math.random() * 4) % 2 ? 1 : -1;
+  }
+  return behavior;
+};
 
-    function setupBuffer($) {
-        $.buffer = new Uint8Array($.width * $.height).fill(0);
-    }
+const calculatePixel = ($, idx, decay, behavior) => {
+  const paletteEnd = $.palette.length - 1;
+  let position = idx + $.width;
+  const reading = $.buffer[position] - decay;
 
-    function setupSeed ($) {
-        var i;
+  if ((position % $.width) < $.width - 1 || (position % $.height) < $.height - 1) {
+    position += (behavior * decay);
+  }
 
-        for (i = $.buffer.length - $.width; i < $.buffer.length; i++) {
-            $.buffer[i] = $.palette.length - 1;
-        }
-    };
+  if (reading < 0) {
+    return 0;
+  }
+  if (reading > paletteEnd) {
+    return paletteEnd;
+  }
+  return reading;
+};
 
-    function propagate($, direction) {
-        if (direction != 1 && direction != -1) {
-            direction = 0;
-        }
-        
-        for (var i = $.buffer.length - $.width - 1; i >= 0; i--) {
-            $.buffer[i] = calculatePixel(
-                $,
-                i,
-                calculateDecay(),
-                calculateBehavior(direction));
-        }
-    }
+const propagate = ($, direction) => {
+  if (direction !== 1 && direction !== -1) {
+    direction = 0;
+  }
 
-    function renderBuffer ($) {
-        $.screen.putImageData(createImageDataFromBuffer($), 0, 0);
-    }
+  for (let i = $.buffer.length - $.width - 1; i >= 0; i -= 1) {
+    $.buffer[i] = calculatePixel(
+      $,
+      i,
+      calculateDecay(),
+      calculateBehavior(direction)
+    );
+  }
+};
 
-    function createImageDataFromBuffer ($) {
-        var color, image = $.screen.createImageData($.width, $.height);
+const createImageDataFromBuffer = ($) => {
+  let color;
+  const image = $.screen.createImageData($.width, $.height);
 
-        for (var i = 0, j = 0; i < $.buffer.length; i++, j = i*4) {
-            color = $.palette[$.buffer[i]];
+  for (let i = 0, j = 0; i < $.buffer.length; i += 1, j = i * 4) {
+    color = $.palette[$.buffer[i]];
 
-            image.data[j + 0] = (color >> 16) & 0xFF;
-            image.data[j + 1] = (color >> 8) & 0xFF;
-            image.data[j + 2] = (color >> 0) & 0xFF;
-            image.data[j + 3] = 0xFF;
-        }
+    image.data[j + 0] = (color >> 16) & 0xFF;
+    image.data[j + 1] = (color >> 8) & 0xFF;
+    image.data[j + 2] = (color >> 0) & 0xFF;
+    image.data[j + 3] = 0xFF;
+  }
 
-        return image;
-    };
+  return image;
+};
 
-    function calculateDecay(reduction) {
-        if (!reduction) {
-            reduction = .15;
-        }
+const renderBuffer = ($) => {
+  $.screen.putImageData(createImageDataFromBuffer($), 0, 0);
+};
 
-        return Math.floor(Math.random() * (1+reduction));
-    }
+const mainLoop = () => {
+  propagate(ROOT, 0);
+  renderBuffer(ROOT);
+};
 
-    function calculateBehavior(direction) {
-        behavior = direction;
-        if (direction == 0) {
-            behavior = Math.floor(Math.random() * 4) % 2 ? 1 : -1;
-        }
-
-        return behavior;
-    }
-    
-    function calculatePixel($, i, decay, behavior) {
-        var reading, position, paletteEnd = $.palette.length - 1;
-
-        position = i + $.width;
-        if ((position % $.width) < $.width-1 || (position % $.height) < $.height-1) {
-            position += (behavior * decay);
-        }
-        reading = $.buffer[position] - decay;
-
-        if (reading < 0) {
-            return 0;
-        }
-
-        if (reading > paletteEnd) {
-            return paletteEnd;
-        }
-
-        return reading;
-    }
-})();
+window.addEventListener('load', () => {
+  setup();
+  window.mainLoopInterval = window.setInterval(mainLoop, 80);
+});
